e9c66103fbc2a661a0f0fdc0e5a5f5c4d398c5df
0xFEEDC0DE64 <daniel@brunner.ninja>
2019-06-22 19:54:01 +0200

    Style cleanups

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <QApplication>
-#include <QStyleFactory>
 #include <QPalette>
 
 #include "widgets/mainwindow.h"
@@ -7,8 +6,6 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
-    app.setStyle(QStyleFactory::create("Fusion"));
 
     QPalette darkPalette;
     darkPalette.setColor(QPalette::Window, QColor(53,53,53));
@@ -22,17 +19,15 @@ int main(int argc, char *argv[])
     darkPalette.setColor(QPalette::ButtonText, Qt::white);
     darkPalette.setColor(QPalette::BrightText, Qt::red);
     darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-
     darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
     darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-
     app.setPalette(darkPalette);
 
     app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
     MainWindow mainWindow;
     mainWindow.resize(1366, 768);
-    mainWindow.show();
+    mainWindow.showMaximized();
 
     return app.exec();
 }
